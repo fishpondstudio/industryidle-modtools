f9df783b3da8073721a387ee27614bc1148d80d7
insraq <ruoysun@gmail.com>
2021-05-18 01:38:45 +0300

    Update

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { Component } from "preact";
-import { getUrlParams } from "./helper";
+import { getUrlParams, nf } from "./helper";
 
-export class App extends Component<{}, { data: any }> {
+export class App extends Component<{}, { chats: any; trades: any }> {
     constructor() {
         super();
         this.loadData();
@@ -11,7 +11,12 @@ export class App extends Component<{}, { data: any }> {
         fetch(`https://api.fishpondstudio.com/stat?token=${getUrlParams()?.token}`)
             .then((r) => r.json())
             .then((j) => {
-                this.setState({ data: j });
+                this.setState({ chats: j });
+            });
+        fetch(`https://api.fishpondstudio.com/trade?token=${getUrlParams()?.token}`)
+            .then((r) => r.json())
+            .then((j) => {
+                this.setState({ trades: j });
             });
     };
 
@@ -25,69 +30,153 @@ export class App extends Component<{}, { data: any }> {
         ).then(this.loadData);
     };
 
+    deleteTrade = (userId: string) => {
+        if (window.confirm("Are you sure to delete this trade?")) {
+            fetch(`https://api.fishpondstudio.com/trade?token=${getUrlParams()?.token}&userId=${userId}`).then(
+                this.loadData
+            );
+        }
+    };
+
     render() {
-        if (!this.state.data) {
+        if (!this.state.chats || !this.state.trades) {
             return;
         }
-        const hasChat = this.state.data.hasChat;
-        const hasBan = this.state.data.hasBan;
+        const hasChat = this.state.chats.hasChat;
+        const hasBan = this.state.chats.hasBan;
+        const trades = this.state.trades;
         return (
-            <table>
-                <tr>
-                    <th></th>
-                    <th>Name</th>
-                    <th>Time</th>
-                    <th>Content</th>
-                    <th class="text-center">Ban Chat</th>
-                    <th class="text-center">Ban Trade</th>
-                </tr>
-                {Object.keys(hasChat).map((k) => {
-                    return (
-                        <tr>
-                            <td class="text-right">
-                                [{hasChat[k].message.flag.toUpperCase()}]{hasChat[k].message.dlc ? "[DLC]" : ""}
-                            </td>
-                            <td>{hasChat[k].message.user}</td>
-                            <td>{new Date(hasChat[k].message.time).toLocaleString()}</td>
-                            <td>{hasChat[k].message.message}</td>
-                            <td class="nowrap text-center">
-                                <button onClick={this.ban.bind(this, "banChat", 0, hasChat[k].ip)}>0m</button>{" "}
-                                <button onClick={this.ban.bind(this, "banChat", 5 * 60 * 1000, hasChat[k].ip)}>
-                                    5m
-                                </button>{" "}
-                                <button onClick={this.ban.bind(this, "banChat", 30 * 60 * 1000, hasChat[k].ip)}>
-                                    30m
-                                </button>{" "}
-                                <button onClick={this.ban.bind(this, "banChat", 24 * 60 * 60 * 1000, hasChat[k].ip)}>
-                                    24h
-                                </button>
-                                <div class="red bold text-center">
-                                    {hasBan?.[k]?.banChat
-                                        ? `${Math.round((hasBan?.[k]?.banChat - Date.now()) / 100 / 60) / 10}m Left`
-                                        : ""}
-                                </div>
-                            </td>
-                            <td class="nowrap text-center">
-                                <button onClick={this.ban.bind(this, "banTrade", 0, hasChat[k].ip)}>0m</button>{" "}
-                                <button onClick={this.ban.bind(this, "banTrade", 5 * 60 * 1000, hasChat[k].ip)}>
-                                    5m
-                                </button>{" "}
-                                <button onClick={this.ban.bind(this, "banTrade", 30 * 60 * 1000, hasChat[k].ip)}>
-                                    30m
-                                </button>{" "}
-                                <button onClick={this.ban.bind(this, "banTrade", 24 * 60 * 60 * 1000, hasChat[k].ip)}>
-                                    24h
-                                </button>
-                                <div class="red bold text-center">
-                                    {hasBan?.[k]?.banTrade
-                                        ? `${Math.round((hasBan?.[k]?.banTrade - Date.now()) / 100 / 60) / 10}m Left`
-                                        : ""}
-                                </div>
-                            </td>
-                        </tr>
-                    );
-                })}
-            </table>
+            <>
+                <table>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Time</th>
+                        <th>Content</th>
+                        <th class="text-right">Ban Chat</th>
+                    </tr>
+                    {Object.keys(hasChat).map((k) => {
+                        return (
+                            <tr>
+                                <td class="text-right">
+                                    [{hasChat[k].message.flag.toUpperCase()}]{hasChat[k].message.dlc ? "[DLC]" : ""}
+                                </td>
+                                <td>{hasChat[k].message.user}</td>
+                                <td>{new Date(hasChat[k].message.time).toLocaleString()}</td>
+                                <td>{hasChat[k].message.message}</td>
+                                <td class="nowrap text-right">
+                                    <button onClick={this.ban.bind(this, "banChat", 0, hasChat[k].ip)}>0m</button>{" "}
+                                    <button onClick={this.ban.bind(this, "banChat", 5 * 60 * 1000, hasChat[k].ip)}>
+                                        5m
+                                    </button>{" "}
+                                    <button onClick={this.ban.bind(this, "banChat", 30 * 60 * 1000, hasChat[k].ip)}>
+                                        30m
+                                    </button>{" "}
+                                    <button
+                                        onClick={this.ban.bind(this, "banChat", 24 * 60 * 60 * 1000, hasChat[k].ip)}
+                                    >
+                                        24h
+                                    </button>
+                                    <div class="red bold">
+                                        {hasBan?.[k]?.banChat
+                                            ? `${Math.round((hasBan?.[k]?.banChat - Date.now()) / 100 / 60) / 10}m Left`
+                                            : ""}
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </table>
+                <table>
+                    <tr>
+                        <th>Side</th>
+                        <th>From</th>
+                        <th>Resource</th>
+                        <th class="text-right">Amount</th>
+                        <th class="text-right">Price</th>
+                        <th class="text-right">Value</th>
+                        <th class="text-right">Quota</th>
+                        <th class="text-right">Valuation</th>
+                        <th class="text-right">Cash</th>
+                        <th class="text-right">CPS</th>
+                        <th class="text-right">Building #</th>
+                        <th class="text-right">Created At</th>
+                        <th class="text-right">Ban Trade</th>
+                    </tr>
+                    {Object.keys(trades).map((k) => {
+                        const trade = trades[k];
+                        const value = trade.price * trade.amount;
+                        const valuation = trade.resourceValuation + trade.buildingValuation;
+                        const quota = (trade.price * trade.amount) / valuation;
+                        return (
+                            <tr>
+                                <td>{trade.side}</td>
+                                <td>{trade.from}</td>
+                                <td>{trade.resource}</td>
+                                <td className={trade.amount > 1e9 ? "red text-right" : "text-right"}>
+                                    {trade.amount} ({nf(trade.amount)})
+                                </td>
+                                <td className="text-right">
+                                    {trade.price} ({nf(trade.price)})
+                                </td>
+                                <td className={value > 1e12 ? "text-right red" : "text-right"}>
+                                    {value} ({nf(value)})
+                                </td>
+                                <td className={quota > 0.01 ? "red text-right" : "text-right"}>
+                                    {Math.round(quota * 100 * 100) / 100}%
+                                </td>
+                                <td className="text-right">
+                                    {valuation} ({nf(valuation)})
+                                </td>
+                                <td className="text-right">
+                                    {trade.cash} ({nf(trade.cash)})
+                                </td>
+                                <td className="text-right">
+                                    {trade.cashPerSec} ({nf(trade.cashPerSec)})
+                                </td>
+                                <td className="text-right">{trade.buildingCount}</td>
+                                <td className="text-right">
+                                    {Math.round((Date.now() - trade.createdAt) / 100 / 60) / 10}m ago
+                                </td>
+                                <td className="nowrap text-right">
+                                    <button onClick={() => navigator.clipboard.writeText(trade.fromUserId)}>
+                                        Copy
+                                    </button>{" "}
+                                    <button onClick={this.ban.bind(this, "banTrade", 0, trade.fromIp)}>0</button>{" "}
+                                    <button
+                                        onClick={this.ban.bind(this, "banTrade", 24 * 60 * 60 * 1000, trade.fromIp)}
+                                    >
+                                        1d
+                                    </button>{" "}
+                                    <button
+                                        onClick={this.ban.bind(this, "banTrade", 7 * 24 * 60 * 60 * 1000, trade.fromIp)}
+                                    >
+                                        1w
+                                    </button>{" "}
+                                    <button
+                                        onClick={this.ban.bind(
+                                            this,
+                                            "banTrade",
+                                            30 * 24 * 60 * 60 * 1000,
+                                            trade.fromIp
+                                        )}
+                                    >
+                                        1mo
+                                    </button>{" "}
+                                    <button onClick={this.deleteTrade.bind(this, trade.fromUserId)}>Delete</button>
+                                    <div class="red bold text-right">
+                                        {hasBan?.[k]?.banTrade
+                                            ? `${
+                                                  Math.round((hasBan?.[k]?.banTrade - Date.now()) / 100 / 60) / 10
+                                              }m Left`
+                                            : ""}
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </table>
+            </>
         );
     }
 }
--- src/helper.ts
+++ src/helper.ts
@@ -7,3 +7,22 @@ export function getUrlParams(): Record<string, string> {
     });
     return result;
 }
+
+// prettier-ignore
+const NUMBER_SUFFIX_1 = ["", "K", "M", "B", "T", "Qa", "Qt", "Sx", "Sp", "Oc", "Nn", "Dc", "UDc", "DDc", "TDc", "QaDc", "QtDc", "SxDc", "SpDc", "ODc",
+    "NDc", "Vi", "UVi", "DVi", "TVi", "QaVi", "QtVi", "SxVi", "SpVi", "OcVi", "NnVi", "Tg", "UTg", "DTg", "TTg", "QaTg", "QtTg", "SxTg", "SpTg", "OcTg",
+    "NnTg", "Qd", "UQd", "DQd", "TQd", "QaQd", "QtQd", "SxQd", "SpQd", "OcQd", "NnQd", "Qq", "UQq", "DQq", "TQq", "QaQq", "QtQq", "SxQq", "SpQq", "OcQq",
+    "NnQq", "Sg"];
+
+export function nf(num: number): string {
+    let idx = 0;
+    while (Math.abs(num) >= 1000) {
+        num /= 1000;
+        idx++;
+    }
+    num = Math.round(num * 100) / 100;
+    if (idx < NUMBER_SUFFIX_1.length) {
+        return num.toLocaleString() + NUMBER_SUFFIX_1[idx];
+    }
+    return num.toLocaleString() + "E" + idx;
+}

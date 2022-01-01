8e4eb823f2da8610b24e56b46cb51b9df889505f
Ruoyu Sun <ruoysun@gmail.com>
2022-01-01 17:13:56 +0200

    Update

--- src/ConfigPage.tsx
+++ src/ConfigPage.tsx
@@ -2,69 +2,89 @@ import { Component } from "preact";
 import { API_HOST } from "./Constants";
 import { getUrlParams } from "./Helper";
 
-export class ConfigPage extends Component<{}, { config: string; saving: boolean }> {
-    constructor() {
-        super();
-        fetch(`${API_HOST}/config?token=${getUrlParams()?.token}`)
-            .then((r) => {
-                if (r.status === 200) {
-                    return r.text();
-                } else {
-                    throw new Error(r.status + " " + r.statusText);
-                }
-            })
-            .then((j) => {
-                this.setState({ config: j });
-            });
-    }
-    render() {
-        if (!this.state.config) {
-            return null;
+export class ConfigPage extends Component<
+  {},
+  { config: string; saving: boolean }
+> {
+  private codeMirror?: CodeMirror.Editor;
+
+  constructor() {
+    super();
+    fetch(`${API_HOST}/config?token=${getUrlParams()?.token}`)
+      .then((r) => {
+        if (r.status === 200) {
+          return r.text();
+        } else {
+          throw new Error(r.status + " " + r.statusText);
         }
-        return (
-            <>
-                <textarea
-                    class="edit-config"
-                    onInput={(e) => {
-                        this.setState({ config: (e.target as HTMLTextAreaElement).value });
-                    }}
-                >
-                    {this.state.config}
-                </textarea>
-                <button
-                    disabled={this.state.saving}
-                    onClick={async () => {
-                        this.setState({ saving: true });
-                        try {
-                            await fetch(`${API_HOST}/config?token=${getUrlParams()?.token}`, {
-                                method: "post",
-                                body: this.state.config,
-                                headers: { "Content-Type": "text/plain" },
-                            });
-                            if (confirm("Config saved, open live config?")) {
-                                openLiveConfig();
-                            }
-                        } catch (error) {
-                            alert(error);
-                        } finally {
-                            this.setState({ saving: false });
-                        }
-                    }}
-                >
-                    Save
-                </button>{" "}
-                <button
-                    onClick={() => {
-                        openLiveConfig();
-                    }}
-                >
-                    Live Config
-                </button>
-            </>
-        );
+      })
+      .then((j) => {
+        this.setState({ config: j });
+      });
+  }
+  componentDidUpdate() {
+    if (this.codeMirror) {
+      return;
     }
+    const editor = document.getElementById("editor");
+    if (editor) {
+      this.codeMirror = CodeMirror(editor, {
+        value: this.state.config,
+        mode: "yaml",
+        keyMap: "sublime",
+        lineNumbers: true,
+      });
+    }
+  }
+  render() {
+    if (!this.state.config) {
+      return null;
+    }
+    return (
+      <>
+        <div id="editor"></div>
+        <br />
+        <button
+          disabled={this.state.saving}
+          onClick={async () => {
+            if (!this.codeMirror) {
+              alert("Code Mirror is not initialized!");
+              return;
+            }
+            this.setState({ saving: true });
+            try {
+              await fetch(`${API_HOST}/config?token=${getUrlParams()?.token}`, {
+                method: "post",
+                body: this.codeMirror.getValue(),
+                headers: { "Content-Type": "text/plain" },
+              });
+              if (confirm("Config saved, open live config?")) {
+                openLiveConfig();
+              }
+            } catch (error) {
+              alert(error);
+            } finally {
+              this.setState({ saving: false });
+            }
+          }}
+        >
+          Save
+        </button>{" "}
+        <button
+          onClick={() => {
+            openLiveConfig();
+          }}
+        >
+          Live Config
+        </button>
+      </>
+    );
+  }
 }
 
 function openLiveConfig() {
-    window.open(`${API_HOST}/config-live?token=${getUrlParams()?.token}`, "_blank");
+  window.open(
+    `${API_HOST}/config-live?token=${getUrlParams()?.token}`,
+    "_blank"
+  );
 }
--- src/LeaderboardPage.tsx
+++ src/LeaderboardPage.tsx
@@ -2,151 +2,177 @@ import { Component } from "preact";
 import { getUrlParams, nf } from "./Helper";
 
 interface ILeaderboard {
-    _id: string;
-    allPrestigeCurrency: number;
-    buildingCount: number;
-    buildingValuation: number;
-    cash: number;
-    dlc: boolean;
-    flag: string;
-    map: string;
-    mapCreatedAt: number;
-    prestigeCurrency: number;
-    resourceValuation: number;
-    userName: string;
+  _id: string;
+  allPrestigeCurrency: number;
+  buildingCount: number;
+  buildingValuation: number;
+  cash: number;
+  dlc: boolean;
+  flag: string;
+  map: string;
+  mapCreatedAt: number;
+  prestigeCurrency: number;
+  resourceValuation: number;
+  userName: string;
 }
 
 interface IUserItem {
-    userName: string;
-    valuation: number;
-    rank: number;
-    allPrestigeCurrency: number;
+  userName: string;
+  valuation: number;
+  rank: number;
+  allPrestigeCurrency: number;
 }
 
 export class LeaderboardPage extends Component<
-    {},
-    { leaderboards: { _id: string; _rev: string; updatedAt: number; data: ILeaderboard[] }[] }
+  {},
+  {
+    leaderboards: {
+      _id: string;
+      _rev: string;
+      updatedAt: number;
+      data: ILeaderboard[];
+    }[];
+  }
 > {
-    constructor() {
-        super();
-        this.loadData();
-    }
+  constructor() {
+    super();
+    this.loadData();
+  }
 
-    async loadData() {
-        fetch(`https://couchdb-de.fishpondstudio.com/industryidle_lblog/_find`, {
-            headers: { Authorization: `Basic ${btoa(getUrlParams()?.couchdb)}`, "Content-Type": "application/json" },
-            method: "post",
-            body: JSON.stringify({
-                selector: {},
-                sort: [{ updatedAt: "desc" }],
-                limit: 20,
-            }),
-        })
-            .then((r) => {
-                if (r.status === 200) {
-                    return r.json();
-                } else {
-                    throw new Error(r.status + " " + r.statusText);
-                }
-            })
-            .then((j) => {
-                this.setState({ leaderboards: j.docs });
-            });
-    }
-
-    async deleteLeaderboard(id: string, rev: string) {
-        fetch(`https://couchdb-de.fishpondstudio.com/industryidle_lblog/${id}?rev=${rev}`, {
-            headers: { Authorization: `Basic ${btoa(getUrlParams()?.couchdb)}`, "Content-Type": "application/json" },
-            method: "delete",
-        }).then((r) => {
-            if (r.status === 200) {
-                this.loadData();
-            } else {
-                throw new Error(r.status + " " + r.statusText);
-            }
-        });
-    }
-
-    render() {
-        if (!this.state.leaderboards) {
-            return;
+  async loadData() {
+    fetch(`https://couchdb-de.fishpondstudio.com/industryidle_lblog/_find`, {
+      headers: {
+        Authorization: `Basic ${btoa(getUrlParams()?.couchdb)}`,
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify({
+        selector: {},
+        sort: [{ updatedAt: "desc" }],
+        limit: 20,
+      }),
+    })
+      .then((r) => {
+        if (r.status === 200) {
+          return r.json();
+        } else {
+          throw new Error(r.status + " " + r.statusText);
         }
+      })
+      .then((j) => {
+        this.setState({ leaderboards: j.docs });
+      });
+  }
 
-        const idx: Record<number, Record<string, IUserItem>> = {};
+  async deleteLeaderboard(id: string, rev: string) {
+    fetch(
+      `https://couchdb-de.fishpondstudio.com/industryidle_lblog/${id}?rev=${rev}`,
+      {
+        headers: {
+          Authorization: `Basic ${btoa(getUrlParams()?.couchdb)}`,
+          "Content-Type": "application/json",
+        },
+        method: "delete",
+      }
+    ).then((r) => {
+      if (r.status === 200) {
+        this.loadData();
+      } else {
+        throw new Error(r.status + " " + r.statusText);
+      }
+    });
+  }
 
-        this.state.leaderboards.forEach((l, index) => {
-            if (!idx[index]) {
-                idx[index] = {};
-            }
-            l.data.forEach((f, i) => {
-                idx[index][f._id] = {
-                    userName: f.userName,
-                    valuation: f.resourceValuation + f.buildingValuation,
-                    allPrestigeCurrency: f.allPrestigeCurrency,
-                    rank: i + 1,
-                };
-                [f.userName, f.resourceValuation + f.buildingValuation, i + 1];
-            });
-        });
-
-        return (
-            <table>
-                <tr>
-                    <th></th>
-                    {this.state.leaderboards.map((d) => {
-                        return (
-                            <th
-                                class="text-right pointer"
-                                onClick={() => {
-                                    if (window.confirm(`Do you want to delete ${d._id} (rev: ${d._rev})`)) {
-                                        this.deleteLeaderboard(d._id, d._rev);
-                                    }
-                                }}
-                            >
-                                -{Math.floor((10 * (Date.now() - d.updatedAt)) / (1000 * 60 * 60)) / 10}h
-                            </th>
-                        );
-                    })}
-                </tr>
-                {this.state.leaderboards[0].data.map((d) => {
-                    return (
-                        <tr>
-                            <td
-                                className={d.dlc ? "pointer" : "pointer bold"}
-                                onClick={() => navigator.clipboard.writeText(d._id ?? "")}
-                            >
-                                {d.userName}
-                            </td>
-                            {this.state.leaderboards.map((l, index) => {
-                                const current = idx[index][d._id];
-                                const prev = idx[index + 1] ? idx[index + 1][d._id] : null;
-                                return (
-                                    <>
-                                        <td
-                                            title={
-                                                current
-                                                    ? `Rank: ${nf(current.rank)}, Swiss Money: ${nf(
-                                                          current.allPrestigeCurrency
-                                                      )}`
-                                                    : "N/A"
-                                            }
-                                            class={
-                                                current &&
-                                                prev &&
-                                                Math.abs(current.valuation - prev.valuation) / prev.valuation > 0.1
-                                                    ? "text-right red"
-                                                    : "text-right"
-                                            }
-                                        >
-                                            {current ? nf(current.valuation) : ""}
-                                        </td>
-                                    </>
-                                );
-                            })}
-                        </tr>
-                    );
-                })}
-            </table>
-        );
+  render() {
+    if (!this.state.leaderboards) {
+      return;
     }
+
+    const idx: Record<number, Record<string, IUserItem>> = {};
+
+    this.state.leaderboards.forEach((l, index) => {
+      if (!idx[index]) {
+        idx[index] = {};
+      }
+      l.data.forEach((f, i) => {
+        idx[index][f._id] = {
+          userName: f.userName,
+          valuation: f.resourceValuation + f.buildingValuation,
+          allPrestigeCurrency: f.allPrestigeCurrency,
+          rank: i + 1,
+        };
+        [f.userName, f.resourceValuation + f.buildingValuation, i + 1];
+      });
+    });
+
+    return (
+      <table>
+        <tr>
+          <th></th>
+          {this.state.leaderboards.map((d) => {
+            return (
+              <th
+                class="text-right pointer"
+                onClick={() => {
+                  if (
+                    window.confirm(
+                      `Do you want to delete ${d._id} (rev: ${d._rev})`
+                    )
+                  ) {
+                    this.deleteLeaderboard(d._id, d._rev);
+                  }
+                }}
+              >
+                -
+                {Math.floor(
+                  (10 * (Date.now() - d.updatedAt)) / (1000 * 60 * 60)
+                ) / 10}
+                h
+              </th>
+            );
+          })}
+        </tr>
+        {this.state.leaderboards[0].data.map((d) => {
+          return (
+            <tr>
+              <td
+                className={d.dlc ? "pointer" : "pointer bold"}
+                onClick={() => navigator.clipboard.writeText(d._id ?? "")}
+              >
+                {d.userName}
+              </td>
+              {this.state.leaderboards.map((l, index) => {
+                const current = idx[index][d._id];
+                const prev = idx[index + 1] ? idx[index + 1][d._id] : null;
+                return (
+                  <>
+                    <td
+                      title={
+                        current
+                          ? `Rank: ${nf(current.rank)}, Swiss Money: ${nf(
+                              current.allPrestigeCurrency
+                            )}`
+                          : "N/A"
+                      }
+                      class={
+                        current &&
+                        prev &&
+                        Math.abs(current.valuation - prev.valuation) /
+                          prev.valuation >
+                          0.1
+                          ? "text-right red"
+                          : "text-right"
+                      }
+                    >
+                      {current ? nf(current.valuation) : ""}
+                    </td>
+                  </>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </table>
+    );
+  }
 }
